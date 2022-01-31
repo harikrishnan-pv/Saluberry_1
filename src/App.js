73c500c8e5e34a37a3e5bda9020ce02c43cb8f43
Mr. Robot <harikrznan@gmail.com>
2022-01-31 17:48:29 +0530

    hashroute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Scroll from "./components/scroll";
 import About from "./pages/about";
