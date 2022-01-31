2a270fae210c8b2b28e65e02a42110966f759a7f
Mr. Robot <harikrznan@gmail.com>
2022-01-31 17:57:27 +0530

    links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Scroll from "./components/scroll";
-import About from "./pages/about";
-import Shop from "./pages/shop";
-import Home from "./pages/home";
 import Featured from "./pages/featured";
+import Home from "./pages/home";
+import Shop from "./pages/shop";
 import Auth from "./pages/signin";
 
 function App() {
@@ -16,18 +15,16 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
+          <Route path="/featured">
+            <Featured />
+          </Route>
           <Route path="/shop">
             <Shop />
           </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route  path="/signin">
+          <Route path="/signin">
             <Auth />
           </Route>
-          <Route  path="/featured">
-            <Featured />
-          </Route>
+          
         </Switch>
       </Layout>
     </Router>
